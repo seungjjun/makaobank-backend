ff52999df18473800ed3f2249d2e28efc8160dfc
seungjjun <il1uso@naver.com>
2022-09-27 00:57:09 +0900

    account service / repository 생성 - account 모델 생성

---
+++ makaobank-backend/src/test/java/kr/megaptera/makaobank/services/AccountServiceTest.java
@@ -0,0 +1,39 @@
+package kr.megaptera.makaobank.services;
+
+import kr.megaptera.makaobank.models.Account;
+import kr.megaptera.makaobank.repositories.AccountRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+
+class AccountServiceTest {
+  private AccountService accountService;
+
+  private AccountRepository accountRepository;
+
+  @BeforeEach
+  void setUp() {
+    accountRepository = mock(AccountRepository.class);
+
+    given(accountRepository.findByAccountNumber(any()))
+        .willReturn(Optional.of(Account.fake("1234")));
+
+    accountService = new AccountService(accountRepository);
+  }
+
+  @Test
+  void account() {
+    Account account = accountService.detail("1234");
+
+    verify(accountRepository).findByAccountNumber("1234");
+
+    assertThat(account.getAccountNumber()).isEqualTo("1234");
+  }
+}
